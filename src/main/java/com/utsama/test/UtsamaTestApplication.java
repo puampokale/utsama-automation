213b38427f2b27ab98f4d9cd2f4d99116ae525fd
Piyush Jadhav <Piyush.Jadhav@73strings.com>
2024-12-07 21:27:01 +0530

    first commit

---
+++ src/main/java/com/utsama/test/UtsamaTestApplication.java
@@ -0,0 +1,13 @@
+package com.utsama.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtsamaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtsamaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utsama/test/UtsamaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utsama.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtsamaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
